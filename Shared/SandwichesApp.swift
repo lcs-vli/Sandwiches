c586e0c486653244f4c7d323993ca40cf211c55c
Victoria Li <VLi@lcs.on.ca>
2021-01-20 10:11:23 -0500

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Li, Muchen on 2021/1/20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
