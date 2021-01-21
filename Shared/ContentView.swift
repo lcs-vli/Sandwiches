00778e8129c06dc206a83d2d1772b4d685460bba
Victoria Li <VLi@lcs.on.ca>
2021-01-20 21:13:16 -0500

    created a list with all the sandwich names and images; added the total number of sandwiches; added navigation links so that the app can navigate into different subpages of sandwiches

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -5,17 +5,56 @@
 //  Created by Li, Muchen on 2021/1/20.
 //
 
+//shortcuts:
+//  command click (VStack)
+//  option + command click (inspector)
+
+
 import SwiftUI
 
 struct ContentView: View {
+    var sandwiches: [Sandwich] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(sandwiches) { sandwich in
+                    SandwichCell(sandwich: sandwich)
+                }
+                
+                HStack {
+                    Spacer()
+                    Text("\(sandwiches.count) Sandwiches")
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+            }
+            .navigationTitle("Sandwiches")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sandwiches: testData)
+    }
+}
+
+struct SandwichCell: View {
+    var sandwich: Sandwich
+    
+    var body: some View {
+        NavigationLink(destination: Text(sandwich.name)){
+            Image(sandwich.thumbnailName)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(sandwich.name)
+                Text("\(sandwich.ingredientCount) ingredients")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
---
+++ Shared/SandwichDetail.swift
@@ -0,0 +1,20 @@
+//
+//  SandwichDetail.swift
+//  Sandwiches
+//
+//  Created by Li, Muchen on 2021/1/20.
+//
+
+import SwiftUI
+
+struct SandwichDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SandwichDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        SandwichDetail()
+    }
+}
